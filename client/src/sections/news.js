241854b69fcda0fa14ad017f6ff9fc6ab9ce9610
Kingshuk Deb <74722462+Kingshuk-Deb@users.noreply.github.com>
2021-12-17 18:57:15 +0530

    Update news.js

--- client/src/sections/news.js
+++ client/src/sections/news.js
@@ -7,36 +7,32 @@ import thumb1 from 'assets/images/news/pexels-alesia-kozik-6770775.jpeg';
 import thumb2 from 'assets/images/news/pexels-david-mcbee-730564.jpeg';
 import thumb3 from 'assets/images/news/pexels-olya-kobruseva-8358040.jpeg';
 import thumb4 from 'assets/images/news/pexels-pratikxox-4025825.jpeg';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const masonryOptions = { originTop: true };
 
 const News = () => {
   const [data, setData] = useState([]);
-  try {
-    fetch('http://cryptolytical.herokuapp.com/api/cryptonews?limit=5', {
+  
+  useEffect(async () => {
+    const response = await fetch('http://cryptolytical.herokuapp.com/api/cryptonews?limit=5', {
       method: "GET",
       headers: {
         "Authorization": "Bearer Token",
       },
-    }).then(function(response) {
-      return response.json();
-    }).then(function(newsData) {
-      newsData.result.map((news) => {
-        if(news.id === 1 || news.id === 4) news.showDescription = true;
-        else news.showDescription = false;
-        if(news.id === 1) news.thumbnail = thumb1;
-        if(news.id === 2) news.thumbnail = null;
-        if(news.id === 3) news.thumbnail = thumb3;
-        if(news.id === 4) news.thumbnail = thumb2;
-        if(news.id === 5) news.thumbnail = thumb4;
-      })
-      setData(newsData.result);
+    });
+    const json = await response.json();
+    json.result.map((news) => {
+      if(news.id === 1 || news.id === 4) news.showDescription = true;
+      else news.showDescription = false;
+      if(news.id === 1) news.thumbnail = thumb1;
+      if(news.id === 2) news.thumbnail = null;
+      if(news.id === 3) news.thumbnail = thumb3;
+      if(news.id === 4) news.thumbnail = thumb2;
+      if(news.id === 5) news.thumbnail = thumb4;
     })
-  } catch (e) {
-    console.error(e);
-    console.log("there as an error");
-  }
+    setData(json.result);
+  }, []);
 
   return (
     <Box as="section" sx={styles.section}>
